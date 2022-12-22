a225b0848b99f59473f0e54f65b72532155db4c5
kishan-savaliya <kishan211.rejoice@gmail.com>
2022-12-22 18:31:51 +0530

    added file

--- newapp/snippets/models.py
+++ newapp/snippets/models.py
@@ -11,7 +11,7 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(default='python',max_length=100)
-    owner = models.ForeignKey('auth.User',related_name='snippet',on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User',related_name='snippets',on_delete=models.CASCADE)
 
     class Meta:
 
